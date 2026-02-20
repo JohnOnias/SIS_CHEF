270caa2bbcb2bf3116696cfa6e747669c27cfcc0
JohnOnias <john.emanuel.onias07@aluno.ifce.edu.br>
2026-02-20 16:53:10 -0300

    criando pagina de mesas

--- FRONT/src/pages/mesas/MesasView.jsx
+++ FRONT/src/pages/mesas/MesasView.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import "./style/mesas.css";
 
 export default function MesasView() {
+  const [mesas, setMesas] = useState([]);
+
+  useEffect(() => {
+    async function carregarMesas() {
+      const dados = await window.api.mesas.listarMesas();
+      setMesas(dados || []);
+    }
+
+    carregarMesas();
+  }, []);
+
   return (
     <>
-        <h1>Mesas View</h1>
+    <div className="containerMesas">
+      {mesas.length > 0 ? (
+        mesas.map((mesa) => (
+          <div className="mesas" key={mesa.id}>
+            <p className="numero-mesas">
+              <strong>{mesa.numero}</strong>
+            </p>
+
+            <p
+              className="mesas-status"
+              style={{
+                backgroundColor:
+                  mesa.status === "disponivel"
+                    ? "green"
+                    : mesa.status === "ocupada"
+                    ? "red"
+                    : "gray",
+                color: "white", // para o texto ficar visível
+                padding: "5px 10px",
+                borderRadius: "5px"
+              }}
+            >
+              {mesa.status}
+            </p>
+          </div>
+        ))
+      ) : (
+        <div>
+          <h1>Nem Uma Mesa cadastrada!</h1>
+        </div>
+      )}
+      </div>
     </>
-  )
+  );
 }
