9f489170801ad41fb5fa84d32f348bc4eca087ad
JohnOnias <john.emanuel.onias07@aluno.ifce.edu.br>
2026-02-24 16:29:13 -0300

    ajustes

--- FRONT/src/routers/router.jsx
+++ FRONT/src/routers/router.jsx
@@ -10,6 +10,7 @@ import HistoricoView from "../pages/historico/HistoricoView.jsx";
 import CadastrosView from "../pages/cadastros/CadastrosView.jsx"; 
 import ProdutosView from "../pages/produtos/ProdutosView.jsx"
 import MesasView from "../pages/mesas/MesasView.jsx";
+import TelaMesa from "../pages/mesas/telapedido.jsx";
 
 
 const router = createHashRouter([
@@ -52,6 +53,11 @@ const router = createHashRouter([
         path:"mesas",
         element: <MesasView/>
       }
+       {
+        path:"tela",
+        element: <TelaMesa/>
+      }
+
 
     ],
   },
